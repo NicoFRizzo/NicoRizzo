7f94424b3239dda1e870daa6cdb35671de5e8b21
Catriel-Acevey <56076984+Catriel-Acevey@users.noreply.github.com>
2019-11-01 20:48:17 -0300

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-import LandingPage from './screen/LandingPage';
+import Navbar from './screen/Navbar'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Landing from './screen/LandingPage'
+import Account from './screen/Account'
+import LogIn from './screen/LogIn'
+
 
 function App() {
   return (
-    <div>
-      <LandingPage class="div-all"/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+        <Route path='/Landing' commponent={Landing} />
+        <Route path='/Account' commponent={Account} />
+        <Route path='/LogIn' commponent={LogIn} />
+      </div>
+    </BrowserRouter>
   );
 }
 
